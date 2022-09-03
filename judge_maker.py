145378b41db802e76e896aa2a47100c0ba803b6e
root <root@605-14.localdomain>
2022-09-03 14:50:35 +0900

    commit

---
+++ judge_maker.py
@@ -0,0 +1,54 @@
+from audioop import avg
+from typing import Tuple
+from unittest import result
+
+
+class name:
+
+    def make_jedge(self, grade, points):
+        
+        isout = False   #不合格
+        isretest = False    #再テスト
+        count = 0
+
+        for point in points:
+            if point < 10:
+                isout = True
+            if point <= 30:
+                count += 1
+        
+        if count >= 3:
+            isretest = True
+
+        if isout == True:
+
+                result = "3"
+                return result
+
+        elif isretest == True:
+
+                result = "2"
+                return result
+
+        else:
+            if grade == "A" or grade == "B" or grade == "C":
+
+                result = "1"
+                return result
+
+            elif grade == "D":
+
+                result = "2"
+                return result
+
+            elif grade == "E":
+
+                result = "3"
+                return result
+
+points = [0,10,20,30,40,50,60,70,80,90]
+judge = ["A","B","C","D","E"]
+grade = judge[0]
+
+
+print()
